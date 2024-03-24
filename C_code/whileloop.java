e3b077143921a5e555627642af129c5a8977afca
shrutiparsodkar02 <2023bit507@sggs.ac.in>
2024-03-24 17:31:51 +0530

    adding loop codes

---
+++ C_code/whileloop.java
@@ -0,0 +1,11 @@
+public class whileloop {
+    public static void main(String[] args) {
+        int i=3;
+        while(i<10){
+            System.out.println(i);
+            i++;
+
+        }
+    }
+    
+}
