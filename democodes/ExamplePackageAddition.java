8e33991ba9d35d1f8aa3ceec87b12266e24b24b9
shrutiparsodkar02 <2023bit507@sggs.ac.in>
2024-05-07 23:54:23 +0530

    adding package related code

---
+++ democodes/ExamplePackageAddition.java
@@ -0,0 +1,12 @@
+package packageExample;
+public class Addition{
+	private int num1;
+	private int num2;
+	Addition(int n1,int n2){
+		num1=n1;
+		num2=n2;
+	}
+	public int add(){
+		return num1+num2;
+	}
+}
---
+++ democodes/packageimplement.java
@@ -0,0 +1,10 @@
+import packageExample.Addition;
+class sum{
+	public static void main(String args[]){
+		Addition ad=new Addition(10,20);
+		sum s=new sum();
+		System.out.println(ad.add());
+		//System.out.println(s.add());
+	}
+
+}
